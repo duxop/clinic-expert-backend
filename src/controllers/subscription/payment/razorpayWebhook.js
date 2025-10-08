0ed77698d13440df1757f9fa3788283abf449f5c
duxop <sangwan.shivam80@gmail.com>
2025-10-08 21:43:06 +0530

    refactored webhook and added transaction for db calls

--- src/controllers/subscription/payment/razorpayWebhook.js
+++ src/controllers/subscription/payment/razorpayWebhook.js
@@ -25,13 +25,15 @@ const razorpayWebhook = async (req, res) => {
       if (
         body.event === "payment.captured" &&
         body.payload.payment.entity.notes.length === 0
-      )
-        res.status(200).json({ message: "Webhook verified" });
+      ) {
+        return res.status(200).json({ message: "Webhook verified" });
+      }
 
       const { notes } =
         body.event === "payment.captured"
           ? body.payload.payment.entity
           : body.payload.subscription.entity;
+
       const { amount, currency, order_id, id } = body.payload.payment.entity;
 
       let { clinicId, planId, monthly } = notes;
@@ -40,77 +42,98 @@ const razorpayWebhook = async (req, res) => {
       planId = parseInt(planId);
       monthly = monthly === "1";
 
-      const currentSubscription = await prisma.Subscription.findFirst({
-        where: {
-          clinicId,
-          status: "ACTIVE",
-          endDate: { gte: new Date() },
-        },
-        include: {
-          SubscriptionPlan: true,
-          Payment: true,
-        },
-      });
+      await prisma
+        .$transaction(async (prisma) => {
+          const currentSubscription = await prisma.Subscription.findFirst({
+            where: {
+              clinicId,
+              status: "ACTIVE",
+              endDate: { gte: new Date() },
+            },
+          });
 
-      const planPayedFor = await prisma.SubscriptionPlan.findFirst({
-        where: {
-          id: planId,
-          isActive: true,
-        },
-      });
+          const planPayedFor = await prisma.SubscriptionPlan.findFirst({
+            where: {
+              id: planId,
+              isActive: true,
+            },
+          });
 
-      const baseDate = currentSubscription
-        ? new Date(currentSubscription.endDate)
-        : new Date();
-      baseDate.setDate(baseDate.getDate() + 30 * (monthly ? 1 : 12));
-      const endDate = baseDate;
+          const paymentCaptured = await prisma.Payment.findUnique({
+            where: {
+              payment_id: id,
+            },
+          });
 
-      console.log("endDate", endDate);
+          if (paymentCaptured) {
+            throw new Error("Payment already captured");
+          }
 
-      if (!currentSubscription) {
-        if (!planPayedFor)
-          return res.status(404).json({ error: "Plan not found" });
+          const baseDate = currentSubscription
+            ? new Date(currentSubscription.endDate)
+            : new Date();
+          baseDate.setDate(baseDate.getDate() + 30 * (monthly ? 1 : 12));
+          const endDate = baseDate;
 
-        const subscription = await prisma.Subscription.create({
-          data: {
-            clinicId,
-            planId,
-            status: "ACTIVE",
-            endDate,
-            isTrial: false,
-            isMonthly: monthly,
-          },
+          let subscription;
+
+          if (!currentSubscription) {
+            if (!planPayedFor) {
+              throw new Error("Plan not found");
+            }
+
+            subscription = await prisma.Subscription.create({
+              data: {
+                clinicId,
+                planId,
+                status: "ACTIVE",
+                endDate,
+                isTrial: false,
+                isMonthly: monthly,
+              },
+            });
+          } else {
+            subscription = await prisma.Subscription.update({
+              where: {
+                id: currentSubscription.id,
+              },
+              data: {
+                planId,
+                status: "ACTIVE",
+                endDate,
+                isTrial: false,
+                isMonthly: monthly,
+              },
+            });
+          }
+
+          const payment = await prisma.Payment.create({
+            data: {
+              amount,
+              currency,
+              status: "SUCCESS",
+              payment_id: id,
+              order_id,
+              subscriptionId: subscription.id,
+            },
+          });
+
+          console.log("subscription", subscription);
+          console.log("payment", payment);
+        })
+        .then(() => {
+          res.status(200).json({ message: "Webhook verified" });
+        })
+        .catch((err) => {
+          console.error(err);
+          if (err.message === "Payment already captured") {
+            res.status(200).json({ message: err.message });
+          } else if (err.message === "Plan not found") {
+            res.status(404).json({ error: err.message });
+          } else {
+            res.status(500).json({ error: "Internal server error" });
+          }
         });
-        console.log("subscription", subscription);
-
-        return res.status(200).json({ message: "Webhook verified" });
-      }
-
-      const subscription = await prisma.Subscription.update({
-        where: {
-          id: currentSubscription.id,
-        },
-        data: {
-          planId,
-          status: "ACTIVE",
-          endDate,
-          isTrial: false,
-          isMonthly: monthly,
-        },
-      });
-      const payment = await prisma.Payment.create({
-        data: {
-          amount,
-          currency,
-          status: "SUCCESS",
-          payment_id: id,
-          order_id,
-          subscriptionId: subscription.id,
-        },
-      });
-      console.log("subscription", subscription);
-      console.log("payment", payment);
-      return res.status(200).json({ message: "Webhook verified" });
     }
 
     if (body.event === "subscription.authenticated") {
