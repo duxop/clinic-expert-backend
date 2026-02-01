5ae844c2989d1b00da20f8d10bfafd31590c9ded
duxop <sangwan.shivam80@gmail.com>
2026-02-01 19:11:39 +0530

    changed get subscription logic

--- src/controllers/subscription/getSubscription.js
+++ src/controllers/subscription/getSubscription.js
@@ -2,15 +2,9 @@ const { prisma } = require("../../config/database");
 
 const getSubscription = async (req, res) => {
   try {
-    const { clinicId } = req.userData.Clinic.id;
-
-    const Subscription = await prisma.Subscription.findFirst({
-      where: {
-        clinicId: clinicId,
-        status: "ACTIVE",
-      }
-    });
-    return res.status(200).json({ Subscription });
+    return res
+      .status(200)
+      .json({ Subscription: req.userData.Clinic.Subscription });
   } catch (error) {
     console.error("Error during getting all patients:", error);
     return res.status(500).json({ error: "Internal server error" });
